ab991db9fd67ab38d3d4c449ecc1170377ea3575
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2025-01-30 13:39:13 +0000

    updated file

---
+++ recipe-book-app/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+// header - h1 h2 logo emojis
+
+function Header() {
+  return (
+    <div className="header">
+      <header className="center"></header>
+      <h1>Recipes!</h1>
+      <p>Recipe book web app</p>
+    </div>
+  );
+}
+
+export default Header;
