d20d69afff84a0607f72dbe296c1d4b7797b07ce
Ashton Bailey <95630289+menice4@users.noreply.github.com>
2025-01-30 16:41:07 +0000

    cards setup

--- src/components/flashcards/cards/flashcard.jsx
+++ src/components/flashcards/cards/flashcard.jsx
@@ -1,29 +1,13 @@
-import { useState } from "react";
+import react from "react";
 import "./flashcard.css";
 
-const Flashcard = ({ question, answer }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleCardClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+const Flashcard = ({id, title}) => {
 
   return (
-    <div
-      className={`flashcard ${isFlipped ? "flipped" : ""}`}
-      onClick={handleCardClick}
-    >
-      <div className="flashcard-front" id="card">
-        {!isFlipped && (
-          <div className="flashcard-text">
-            <div className="emoji">🤔</div>
-            <div className="question">{question}</div>
-          </div>
-        )}
-      </div>
-      <div className="flashcard-back">
-        {isFlipped && <div className="flashcard-text">{answer}</div>}
-      </div>
+    <div className="flashcard ">      
+        <div className="Recipe" id= {id}></div>
+          <div className="Recipe__title">{title}</div>
     </div>
   );
 };
--- src/components/flashcards/flashcards.jsx
+++ src/components/flashcards/flashcards.jsx
@@ -1,34 +1,60 @@
+import React, { useState, useEffect } from "react";
 import Flashcard from "./cards/flashcard";
 import "./flashcards.css";
 
-function FlashcardsContainer({ flashcards }) {
+function FlashcardsContainer() {
+  const [recipes, setRecipe] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/recipes");
+        const data = await response.json();
+        setRecipe(data.recipes);
+      } catch (error) {
+        console.error("error fetch data:".error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
+    
+
     <div className="cardContainer">
       <Flashcard
-        question="What is React?"
-        answer="React is a free and open-source front-end JavaScript library!"
-      />
-      <Flashcard
-        question="What is JSX?"
-        answer="JSX is a syntax extension of regular JavaScript and is used to create React elements!"
-      />
-      <Flashcard
-        question="What is a state in React?"
-        answer="The state of a component is an object that holds some information that may change over the lifetime of the component!"
-      />
-      <Flashcard
-        question="What are props in React?"
-        answer="Props are objects that can be used inside of a component which can allow us to pass information!"
-      />
-      {flashcards.map((flashcard, index) => (
-        <Flashcard
-          key={index}
-          question={flashcard.question}
-          answer={flashcard.answer}
-        />
-      ))}
+      key= "1"
+      id= "1"
+      title= "Cheat's fish & chips" />
+          <Flashcard
+      key= "2"
+      id= "2"
+      title= "Halloumi eggy crumpets" />
+          <Flashcard
+      key= "2"
+      id= "2"
+      title= "Special scrambled eggs" />
+          <Flashcard
+      key= "2"
+      id= "2"
+      title= "My favourite speedy sausage pizza" />
+          <Flashcard
+      key= "2"
+      id= "2"
+      title= "Mushroom cannelloni" />
+     
+     
+     
+     
+     
+      {/* {recipes.map((recipe) => (
+        <Flashcard 
+        key={recipe.id}
+        id = {recipe.id}
+        title={ recipe.title}/>
+      ))} */}
     </div>
   );
+  
 }
 
 export default FlashcardsContainer;
