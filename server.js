2d4503ad55395a9d84deaed75aeefc9cf5f1fbea
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2025-01-31 11:07:10 +0000

    final adjustments

--- server.js
+++ server.js
@@ -8,23 +8,3 @@ server.use(router);
 server.listen(5001, () => {
   console.log("JSON Server is running on port 5000");
 });
-
-
-
-// const handleSubmit = async (e) => {
-//   e.preventDefault();
-//   const newRecipe = { title, ingredients, instructions };
-
-//   const response = await fetch("http://localhost:5000/recipes", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(newRecipe),
-//   });
-
-//   if (response.ok) {
-//     dispatch({ type: "ADD_RECIPE", payload: newRecipe });
-//     setTitle("");
-//     setIngredients("");
-//     setInstructions("");
-//   }
-// };
