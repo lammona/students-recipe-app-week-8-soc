493c3c6d1d2de7b18fd61bfa10e5db929f4c1efe
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2025-02-11 12:03:48 +0000

    added new url for sentry

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["http://localhost:5173/"],
+  tracePropagationTargets: ["https://reciepe-book-web-app-phi.vercel.app/"],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
